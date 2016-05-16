ac1ceefde8b7b372b7780b48834c7342af2fb5d1
Nikhil Cherukuri <nikhilcherukuri@nikhils-macbook-pro.local>
2016-05-16 14:21:04 -0700

    Initial Commit

---
+++ CheckIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheckIn
+//
+//  Created by Nikhil Cherukuri on 5/16/16.
+//  Copyright © 2016 Nikhil Cherukuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CheckIn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckIn
+//
+//  Created by Nikhil Cherukuri on 5/16/16.
+//  Copyright © 2016 Nikhil Cherukuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
