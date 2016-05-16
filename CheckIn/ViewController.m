e1a5e9adc8ac9edbe7b09168c154ea4ffa4f384a
Jeffrey Vaudrin-McLean <jeffreyvaudrin-mclean@jeffreys-macbook-air.local>
2016-05-16 14:39:00 -0700

    first commit to confirm github repo established properly

--- CheckIn/ViewController.m
+++ CheckIn/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     //this is a comment
+    // this is also a comment
 }
 
 - (void)didReceiveMemoryWarning {
