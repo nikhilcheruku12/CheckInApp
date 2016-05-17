7de0c502f4836a9e6bbf85d4a8d320357c3e40e6
Nikhil Cherukuri <nikhilcherukuri@nikhils-macbook-pro.local>
2016-05-17 14:00:34 -0700

    Made welcome page

--- CheckIn/MainViewController.m
+++ CheckIn/MainViewController.m
@@ -6,13 +6,13 @@
 //  Copyright © 2016 Nikhil Cherukuri. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
